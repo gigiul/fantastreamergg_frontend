f321f75ab078f895905902ba23bd1a1b19cff941
gigiul <luigidalleaste@gmail.com>
2022-12-28 18:14:18 +0100

    created post request for a SIGNUP form, it works correctly with mysql. TO DO: SignIN form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,33 +5,41 @@ import { useEffect } from 'react'
 
 const Login = () => {
 
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     const [data, setData] = useState([]);
-    const [click, setClick] = useState(false);
 
-    const handleClick = () => {
-        setClick(!click);
+    const postData = (e) => {
+        e.preventDefault();
+        Axios.post('http://localhost:3000/user', {
+            name,
+            email,
+            password
+        }).then(res => console.log('posting data', res)).catch(err => console.log(err));
     }
 
-    const arr = data.map((data, index) => {
-        return (
-            <p className='flex flex-col text-center justify-center uppercase'>{data.name}</p>
-        )
-    })
-
-    useEffect(() => {
+{/*    get data request from backend
+        useEffect(() => {
         Axios.get('http://localhost:3000/user').then(res => {console.log(res.data); setData(res.data)}).catch(err => console.log(err));
-    }, []);
+    }, []);*/}
 
   return (
     <div>
         <Navbar />
         <div className='flex flex-col justify-center items-center h-screen'>
             <div className='text-white'>
-                {click ? <h1>{arr}</h1>
-                 : <></>}
-            </div>
-            {!click ? <button onClick={handleClick} className='text-white '>Show Data</button> : <button onClick={handleClick} className='text-white '>Close Data</button>}
-            
+                <form className='flex flex-col justify-center items-center'>
+                    <h1 className='text-4xl uppercase'>Sign Up</h1>
+                    <input onChange={(e) => setName(e.target.value)} type='text' value={name} placeholder='Name' className='bg-gray-800 text-white w-80 h-10 rounded-md mt-4 hover:bg-gray-900' />
+                    <input onChange={(e) => setEmail(e.target.value)} type='text' value={email} placeholder='Email' className='bg-gray-800 text-white w-80 h-10 rounded-md mt-4 hover:bg-gray-900' />
+                    <input onChange={(e) => setPassword(e.target.value)} type='password' value={password} placeholder='Password' className='bg-gray-800 text-white w-80 h-10 rounded-md mt-4 hover:bg-gray-900' />
+                    <button onClick={postData} className='border-gray-700 border-2 bg-gray-800 text-white w-80 h-10 rounded-md mt-4 hover:bg-gray-900'>Sign Up</button>
+                </form>
+                <div className='flex justify-end pt-1'>
+                    <p className='text-sm'>Already registered? <a href='/login'> Sign In</a></p>
+                </div>
+        </div>
         </div>
     </div>
   )
